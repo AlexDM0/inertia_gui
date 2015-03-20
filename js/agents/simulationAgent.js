af780a6561ea69adc08f8e376611425c2545197c
Rick van Krevelen <krevelen@gmail.com>
2015-03-20 15:16:40 +0100

    minor changes

--- js/agents/simulationAgent.js
+++ js/agents/simulationAgent.js
@@ -13,6 +13,7 @@ function SimulationAgent(id) {
   this.LCHid = 0;
   this.DERid = 0;
   this.LCHdata = {};
+  this.DERtemplates = {};
 }
 
 
@@ -25,16 +26,23 @@ SimulationAgent.prototype.rpcFunctions = {};
 
 // get initial values for buttons and color them accordingly
 SimulationAgent.prototype.init = function() {
-  // references to the explaination spans
+  // references to the explanation spans
 
+};
+
+SimulationAgent.prototype.loadDERTemplates = function() {
+	var me = this;
+	this.rpc.request(EVE_ADDRESS, {method:"loadDERTemplates", params: {}}).then(function(reply) {
+		me.DERtemplates = reply;
+	});
 };
 
 
 /**
- * This is called by the start sim button, the ders (if available) are async collected due to the file attachements
- * this is done with promises.
- *
- * */
+ * This is called by the start sim button, the ders (if available) are async
+ * collected due to the file attachements this is done with promises.
+ * 
+ */
 SimulationAgent.prototype.startSimulation = function() {
   // show the overlay
   document.getElementById('progressBarWrapper').style.display = "block";
@@ -70,7 +78,8 @@ SimulationAgent.prototype.sendSimulationData = function() {
 
 
 /**
- * Ony by one parse the LCH's. this is done to allow the async handling of files for ders.
+ * Ony by one parse the LCH's. this is done to allow the async handling of files
+ * for ders.
  */
 SimulationAgent.prototype.parseLCH = function() {
   // finished, send data
@@ -110,8 +119,8 @@ SimulationAgent.prototype.parseLCH = function() {
 
 /**
  * Handle the async collecting of files with promises
- *
- * */
+ * 
+ */
 SimulationAgent.prototype.parseDER = function(LCHid,DERid) {
   return new Promise(function (resolve, reject) {
     this.simData.LCHs[LCHid]['DERs'][DERid] = {id: DERid};
@@ -145,24 +154,63 @@ SimulationAgent.prototype.parseDER = function(LCHid,DERid) {
 
 
 
-//* UTIL functions *//
+// * UTIL functions *//
 
 // remove LCH from data and DOM
 SimulationAgent.prototype.removeLCH = function(id) {
+  if(!confirm('Are you sure, permanently delete setup of LCH '+id+'?')) return;
   var div = document.getElementById(id + "_container")
   div.parentNode.removeChild(div);
   delete this.LCHdata[id];
 }
 
-// add DER in DOM and data
+//add DER in DOM and data
+SimulationAgent.prototype.resetDERFromTemplate = function(areaId, tplId) {
+	  var area = document.getElementById(areaId);
+	  area.setValue(this.DERTemplates[tplId]);
+}
+
+//add DER in DOM and data
 SimulationAgent.prototype.addDER = function(id) {
   var container = document.getElementById(id + "_DERcontainer");
   var div = document.createElement("div");
-  var content = '<img class="delete" src="./images/delete.png" onclick="simAgent.removeDER('+id+','+this.DERid+')" />' +
-    '<table class="DERtable">' +
-    '<tr><td>ID</td><td>' + this.DERid + '</td></tr>' +
-    '<tr><td>data</td><td><textarea id="lch_' + id + '_' + this.DERid + '_kv"></textarea></td></tr>' +
-    '<tr><td>file (optional)</td><td><input type="file" id="' + this.DERid + '_file"></td></tr>' +
+  var areaID = 'lch_' + id + '_' + this.DERid + '_kv';
+  var tplButtons = '&lArr; template: <select onChange="resetDERFromTemplate(\''+areaID+'\',this.value);">';
+  for(var i in {'Lights':'bla','HVACs':'bla','Appliances':'bla','Generators':'bla'})
+  {
+	  tplButtons += '<optgroup label="'+i+'">';
+	  for(var j in {'Small':'bla','Medium':'bla','Large':'bla'})
+		  tplButtons += '<option value="['+i+']['+j+']">\t\t'+j+'</option>';
+	  tplButtons += '</optgroup>';
+  }
+  tplButtons += '</select>';
+  var content = 
+    '<table class="DERtable" width="100%">' +
+    '<tr><th class="leftColumn"><nobr>DER '+this.DERid+'</nobr></th><td align="right">' + tplButtons + '</td>'+
+    '<td width="20" rowSpan="3" vAlign=bottom> <div class="typeButton" title="Remove DER '+this.DERid+'" onClick="simAgent.removeDER('+id+','+this.DERid+')"><img class="delete" src="./images/delete.png"/></div></td></tr>'+
+    '<tr><th class="leftColumn"></th><td><textarea id="'+areaID+'">{\n'+
+    '\t"class": "eu.inertia.model.lch.VirtualDER",\n'+
+    '\t"der-model": { \n'+
+    '\t\t"type": "lighting", \n'+
+    '\t\t"parameters": { ... } \n'+
+    '\t\t"schedule": { ... } \n'+
+    '\t},\n'+
+    '\t"sensors": [ \n'+
+    '\t\t{ \n'+
+    '\t\t\t"type": "power", \n'+
+    '\t\t\t"parameters": { ... } \n'+
+    '\t\t},\n'+
+    '\t\t{ \n'+
+    '\t\t\t"type": "temperature", \n'+
+    '\t\t\t"parameters": { ... } \n'+
+    '\t\t},\n'+
+    '\t\t{ \n'+
+    '\t\t\t"type": "power", \n'+
+    '\t\t\t"parameters": { ... } \n'+
+    '\t\t},\n'+
+    '\t},\n'+
+    '}</textarea></td></tr>' +
+    '<tr><th class="leftColumn">Attach file<br/>(optional)</th><td><input type="file" id="' + this.DERid + '_file"></td></tr>' +
     '</table>';
 
   div.id = 'lch_' + id + '_' + this.DERid + '_div';
@@ -176,6 +224,7 @@ SimulationAgent.prototype.addDER = function(id) {
 
 // removce DER from data and DOM
 SimulationAgent.prototype.removeDER = function(LCHid, DERid) {
+	  if(!confirm('Are you sure, permanently delete setup of DER '+DERid+' from LCH '+LCHid+'?')) return;
   var div = document.getElementById('lch_' + LCHid + '_' + DERid + '_div')
   div.parentNode.removeChild(div);
   delete this.LCHdata[LCHid]["DERs"][DERid];
@@ -183,15 +232,51 @@ SimulationAgent.prototype.removeDER = function(LCHid, DERid) {
 
 
 /**
- *  adds a DOM element to the page for a MESO LCH
- **/
+ * adds a DOM element to the page for a MESO LCH
+ */
 SimulationAgent.prototype.addMesoLCH = function() {
-  var container = document.getElementById("LCHMesoContainer");
+  var container = document.getElementById("LCHContainer");
   var div = document.createElement("div");
-  var content = '<img class="delete" src="./images/delete.png" onclick="simAgent.removeLCH('+this.LCHid+')" />' +
-    '<table class="LCHtable">' +
-    '<tr><td>ID</td><td>' + this.LCHid + '</td></tr>' +
-    '<tr><td>data</td><td><textarea id="lch_' + this.LCHid + '_kv"></textarea></td></tr>' +
+  var areaID = 'lch_' + this.LCHid + '_kv';
+  var tplButtons = '';
+  for(var i in {'SmallArea':'bla','MediumArea':'bla','LargeArea':'bla'})
+	  tplButtons += 
+		    '<button onClick="import">'+i+'</button>';
+  var content = 
+    '<table class="LCHtable" width="100%">' +
+    '<tr><th class="leftColumn"><nobr>LCH ' + this.LCHid + '</nobr></th><td>' + tplButtons + '</td>'+
+    '<td width="20" rowSpan="2" vAlign="bottom"><div class="typeButton" title="Remove LCH '+this.LCHid+'" onClick="simAgent.removeLCH('+this.LCHid+')"><img class="delete" src="./images/delete.png"/></div></td></tr>'+
+    '<tr><th class="leftColumn"></th><td><textarea id="' + areaID + '">{\n'+
+    '\t"class": "eu.inertia.model.lch.VirtualMesoLCH",\n'+
+    '\t"extrapolation-factors": { \n'+
+    '\t\t"lighting" : 2.1, \n'+
+    '\t\t"hvac" : 2.5, \n'+
+    '\t\t"outlets" : 4.0, \n'+
+    '\t\t"generators" : 0.3 \n'+
+    '\t},\n'+
+    '\t"occupancy-day-type-distribution": { \n'+
+    '\t\t"vacant" : 0.1, \n'+
+    '\t\t"minimal" : 0.2, \n'+
+    '\t\t"normal" : 0.4, \n'+
+    '\t\t"maximal" : 0.2, \n'+
+    '\t\t"stressed" : 0.1 \n'+
+    '\t},\n'+
+    '\t"comfort-noise-distribution": { \n'+
+    '\t\t"hourly-extremes" : [ 0.95, 1.05 ], \n'+
+    '\t\t"daily-extremes" : [ 0.95, 1.05 ], \n'+
+    '\t\t"weekly-extremes" : [ 0.95, 1.05 ], \n'+
+    '\t},\n'+
+    '\t"humidity-noise-distribution" : { \n'+
+    '\t\t"hourly-extremes" : [ 0.95, 1.05 ], \n'+
+    '\t\t"daily-extremes" : [ 0.9, 1.1 ], \n'+
+    '\t\t"weekly-extremes" : [ 0.95, 1.05 ], \n'+
+    '\t}, \n'+
+    '\t"temperature-noise-distribution" : { \n'+
+    '\t\t"hourly-extremes" : [ 0.95, 1.05 ], \n'+
+    '\t\t"daily-extremes" : [ 0.9, 1.1 ], \n'+
+    '\t\t"weekly-extremes" : [ 0.95, 1.05 ], \n'+
+    '\t}\n'+
+    '}</textarea></td></tr>' +
     '</table>';
 
   div.id = this.LCHid + "_container"
@@ -205,16 +290,16 @@ SimulationAgent.prototype.addMesoLCH = function() {
 
 
 /**
- *  adds a DOM element to the page for a MICRO LCH
- **/
+ * adds a DOM element to the page for a MICRO LCH
+ */
 SimulationAgent.prototype.addMicroLCH = function() {
-  var container = document.getElementById("LCHMicroContainer");
+  var container = document.getElementById("LCHContainer");
   var div = document.createElement("div");
-  var content = '<img class="delete" src="./images/delete.png" onclick="simAgent.removeLCH('+this.LCHid+')" />' +
-    '<table class="LCHtable">' +
-    '<tr><td>ID</td><td>' + this.LCHid + '</td></tr>' +
-    '<tr><td>DERS</td><td><img src="./images/add.png" onclick="simAgent.addDER(' + this.LCHid + ')" /></td></tr>' +
-    '<tr><td></td><td><div class="DERcontainer" id="' + this.LCHid + '_DERcontainer"></div></td></tr>' +
+  var content = 
+    '<table class="LCHtable" width="100%">' +
+    '<tr><th class="leftColumn">LCH ' + this.LCHid + '</th><td><div class="typeButton" onClick="simAgent.addDER(' + this.LCHid + ')">Add DER <img src="./images/add.png"/></div></td>'+
+    '<td width="20"><div class="typeButton" title="Remove LCH '+this.LCHid+'" onClick="simAgent.removeLCH('+this.LCHid+')"><img class="delete" src="./images/delete.png"/></div></tr>' +
+    '<tr><td colSpan="3"><div class="DERcontainer" id="' + this.LCHid + '_DERcontainer"></div></td></tr>' +
     '</table>';
 
   div.id = this.LCHid + "_container"
